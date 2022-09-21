598ce291ca4d58f236dd20d3e8aa59e84ab72c1e
matheusgrandi <matheus@matheusgrandi.com.br>
2022-09-21 15:39:01 -0300

    :fire: mocks removal

--- test.js
+++
@@ -1,5 +0,0 @@
-import { RelatoriesRepository } from './src/repositories/RelatoriesRepository.js';
-
-const relatoriesRepository = RelatoriesRepository.getInstance();
-
-relatoriesRepository.getDashboardData();
