42703743a1b71dda9b9534841f0bce2c1121bab6
matheusgrandi <matheus@matheusgrandi.com.br>
2022-11-28 16:19:45 -0300

    :bug: cors bug fix

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
-
+import cors from 'cors';
 import { router } from './routes/index.js';
 
-const PORT = 3334;
+const PORT = 3335;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 // app.use(express.json());
